8d26abc834951a4193c885ebdf80174f4603f704
Chinmayee-mohapatra <94803850+Chinmayee-mohapatra@users.noreply.github.com>
2023-11-24 15:02:20 +0530

    basics of cpp

---
+++ Day-01/Basics.cpp
@@ -0,0 +1,19 @@
+/**
+ * C++ syntax
+ * header files
+ * Data types
+ *  - int
+ *  - long
+ *  - long long
+ *  - float
+ *  - double
+ *  - string
+ *  - char
+ * if else statements
+ * switch statement
+ * arrays, string
+ * for loop
+ * while loop
+ * funtions (Pass by Value & Pass by Reference)
+ * 
+*/
